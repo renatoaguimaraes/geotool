f35de5fa3179c8a22ab285ea3fb3e1e87cb72b60
Renato Guimaraes <renato.guimaraes@widescope.pt>
2021-02-25 15:05:03 -0300

    build and run

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t novitatus.com/graphqlgeotool .
+docker build -t novitatus.com/geotool .
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it -p 8085:8085 novitatus.com/geotool geotool
